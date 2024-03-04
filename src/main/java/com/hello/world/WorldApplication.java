e824c8028b7cff8969e799f14e1caf80e18775f9
sayanichopra <Sayanichopra18@gmail.com>
2024-03-04 23:34:56 +0530

    Spring Boot Class added.

---
+++ src/main/java/com/hello/world/WorldApplication.java
@@ -0,0 +1,21 @@
+package com.hello.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello, World!";
+	}
+
+}
